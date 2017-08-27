7d2943154cea55de32b5e1214b499562701839e9
Saied Attallah <saiedattallah@gmail.com>
2017-08-27 17:35:46 +0200

    Setup Friends provider. Setup Map

--- app/src/main/java/com/cosmos/saiedattallah/findfriends/HomeActivity.java
+++ app/src/main/java/com/cosmos/saiedattallah/findfriends/HomeActivity.java
@@ -1,14 +1,238 @@
 package com.cosmos.saiedattallah.findfriends;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 import com.cosmos.saiedattallah.findfriends.injection.BaseActivity;
+import com.cosmos.saiedattallah.findfriends.models.Friend;
+import com.cosmos.saiedattallah.findfriends.providers.FriendsProvider;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class HomeActivity extends BaseActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.inject.Inject;
+
+public class HomeActivity extends BaseActivity implements OnMapReadyCallback, FriendsProvider.OnRetrieveFriendsListListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    @Inject
+    FriendsProvider friendsProvider;
+
+    private static final int REQUEST_CODE_ASK_PERMISSIONS = 132;
+
+    Toolbar toolbar;
+
+    GoogleMap googleMap;
+    SupportMapFragment supportMapFragment;
+    LocationRequest locationRequest;
+    GoogleApiClient googleApiClient;
+    LatLng userLocationLatLng;
+    HashMap<Integer, Marker> markerHashMap;
+
+    ArrayList<Friend> friendsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(this);
+
+        markerHashMap = new HashMap<>();
+        friendsList = new ArrayList<>();
+        friendsProvider.setOnRetrieveFriendsListListener(this);
+        friendsProvider.retrieveFriendsList();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        // show menu only when home fragment is selected
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        this.googleMap.getUiSettings().setCompassEnabled(true);
+        this.googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        buildGoogleApiClient();
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            checkLocationPermission();
+            return;
+        }
+        this.googleMap.setMyLocationEnabled(true);
+        this.googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        //mGoogleMap.setMyLocationEnabled(true);
+        this.googleApiClient.connect();
+
+        this.setupMap();
+
+    }
+
+    @Override
+    public void onFriendsListRetrieved(List<Friend> friendsList) {
+
+    }
+
+    @Override
+    public void onRetrieveFriendsListFailure(int failureReason, int failureMessage) {
+
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        this.googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    private void checkLocationPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                            PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                                android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+                return;
+            }
+        }
+
+    }
+
+    private void setupMap() {
+        this.googleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+
+                View v = getLayoutInflater().inflate(R.layout.info_window, null);
+
+                TextView tvFirstName = (TextView) v.findViewById(R.id.tv_map_friend_first_name);
+
+                try {
+                    int friendId = Integer.parseInt(marker.getTitle());
+                    Friend friend = friendsList.get(friendId);
+
+
+                    LatLng latLng = marker.getPosition();
+                    tvFirstName.setText(friend.getFirstName());
+
+                } catch (NumberFormatException e) {
+
+                }
+                return v;
+            }
+        });
+
+        // this do the trick for loading image on infowindow
+        this.googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+
+            @Override
+            public boolean onMarkerClick(final Marker marker) {
+                marker.showInfoWindow();
+                final Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        marker.showInfoWindow();
+
+                    }
+                }, 200);
+
+                return true;
+            }
+        });
+
+
+        this.googleMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            this.checkLocationPermission();
+            return;
+        }
+            Location userLastLocation = LocationServices.FusedLocationApi.getLastLocation(this.googleApiClient);
+            if (userLastLocation != null) {
+                //place marker at current position
+                //mGoogleMap.clear();
+                userLocationLatLng = new LatLng(userLastLocation.getLatitude(), userLastLocation.getLongitude());
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(userLocationLatLng);
+                markerOptions.title(getString(R.string.current_location));
+                currLocationMarker = googleMap.addMarker(markerOptions);
+
+                //zoom to current position:
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(userLocationLatLng).zoom(17).tilt(0)
+                        .build();
+                this.googleMap.animateCamera(CameraUpdateFactory
+                        .newCameraPosition(cameraPosition));
+            }
+
+        this.locationRequest = new LocationRequest();
+        this.locationRequest.setInterval(100);
+        this.locationRequest.setFastestInterval(100);
+        this.locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        this.locationRequest.setSmallestDisplacement(0.01F);
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
--- app/src/main/java/com/cosmos/saiedattallah/findfriends/providers/FriendsProvider.java
+++ app/src/main/java/com/cosmos/saiedattallah/findfriends/providers/FriendsProvider.java
@@ -1,11 +1,17 @@
 package com.cosmos.saiedattallah.findfriends.providers;
 
+import com.cosmos.saiedattallah.findfriends.R;
 import com.cosmos.saiedattallah.findfriends.models.Friend;
+import com.cosmos.saiedattallah.findfriends.rest.ServerResponse;
 import com.cosmos.saiedattallah.findfriends.rest.WebApiInterface;
 
 import java.util.List;
 
 import javax.inject.Inject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class FriendsProvider {
     @Inject
@@ -13,6 +19,66 @@ public class FriendsProvider {
 
     public OnRetrieveFriendsListListener onRetrieveFriendsListListener;
     private List<Friend> friendsList;
+
+    protected Callback<ServerResponse<List<Friend>>> getFriendsListCallback = new Callback<ServerResponse<List<Friend>>>() {
+        @Override
+        public void onResponse(Call<ServerResponse<List<Friend>>> call, Response<ServerResponse<List<Friend>>> response) {
+            FriendsProvider.this.onRetrieveFriendsListCallbackResponse(call, response);
+        }
+
+        @Override
+        public void onFailure(Call<ServerResponse<List<Friend>>> call, Throwable t) {
+            FriendsProvider.this.onRetrieveFriendsListCallbackFailure(call, t);
+        }
+    };
+
+    public FriendsProvider() {
+        super();
+    }
+
+    public void setOnRetrieveFriendsListListener(OnRetrieveFriendsListListener onRetrieveFriendsListListener) {
+        this.onRetrieveFriendsListListener = onRetrieveFriendsListListener;
+
+    }
+
+    public void removeOnRetrieveFriendsListListener(OnRetrieveFriendsListListener onRetrieveFriendsListListener) {
+        this.onRetrieveFriendsListListener = null;
+    }
+
+    public List<Friend> getFriendsList() {
+        return this.friendsList;
+    }
+
+    public void retrieveFriendsList() {
+            Call<ServerResponse<List<Friend>>> call = this.webApiInterface.getFriendsList("https://www.digi-worx.com/friends.txt");
+            call.enqueue(this.getFriendsListCallback);
+    }
+
+    private void onRetrieveFriendsListCallbackResponse(Call<ServerResponse<List<Friend>>> call, Response<ServerResponse<List<Friend>>> response) {
+        ServerResponse<List<Friend>> serverResponse;
+
+        if (response.isSuccessful()) {
+            serverResponse = response.body();
+            if (serverResponse.getStatus() == ServerResponse.RESPONSE_STATUS_SUCCESS) {
+                this.friendsList = serverResponse.getFriends();
+                if (this.onRetrieveFriendsListListener != null) {
+                    this.onRetrieveFriendsListListener.onFriendsListRetrieved(this.friendsList);
+                }
+            }
+        }
+
+
+        if (this.friendsList == null) {
+            this.onRetrieveFriendsListListener.onRetrieveFriendsListFailure(R.string.find_friends_error, R.string.find_friends_error_msg);
+        }
+
+    }
+
+    private void onRetrieveFriendsListCallbackFailure(Call<ServerResponse<List<Friend>>> call, Throwable t) {
+        this.onRetrieveFriendsListListener.onRetrieveFriendsListFailure(R.string.find_friends_error, R.string.find_friends_error_msg);
+
+
+    }
 
     public interface OnRetrieveFriendsListListener {
         void onFriendsListRetrieved(List<Friend> friendsList);
