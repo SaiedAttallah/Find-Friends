7fc44a7a86328d6f718ede876ed09ddb7b5d8e6f
SaiedAttallah <saiedattallah@gmail.com>
2017-08-30 11:00:35 +0200

    Setup Friends List Activity.

---
+++ app/src/main/java/com/cosmos/saiedattallah/findfriends/FriendsListActivity.java
@@ -0,0 +1,14 @@
+package com.cosmos.saiedattallah.findfriends;
+
+import android.os.Bundle;
+
+import com.cosmos.saiedattallah.findfriends.injection.BaseActivity;
+
+public class FriendsListActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends_list);
+    }
+}
